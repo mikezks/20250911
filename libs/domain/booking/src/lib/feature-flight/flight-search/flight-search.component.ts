ca3ed9a2bf8430a0c2ee1dc84707eccda9940a25
Michael Egger-Zikes <23002661+mikezks@users.noreply.github.com>
2025-09-11 13:37:40 +0200

    init: signal store

--- libs/domain/booking/src/lib/feature-flight/flight-search/flight-search.component.ts
+++ libs/domain/booking/src/lib/feature-flight/flight-search/flight-search.component.ts
@@ -1,25 +1,27 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { Flight, FlightFilter, injectTicketsFacade } from '../../logic-flight';
+import { Flight, FlightFilter } from '../../logic-flight';
+import { FlightService } from '../../logic-flight/data-access/flight.service';
 import { FlightCardComponent, FlightFilterComponent } from '../../ui-flight';
 
 
 @Component({
-  selector: 'app-flight-search',
+  standalone: true,
   imports: [
     CommonModule,
     FormsModule,
     FlightCardComponent,
     FlightFilterComponent
   ],
+  selector: 'app-flight-search',
   templateUrl: './flight-search.component.html',
 })
 export class FlightSearchComponent {
-  private ticketsFacade = injectTicketsFacade();
+  private flightService = inject(FlightService);
 
   protected filter = {
-    from: 'Paris',
+    from: 'London',
     to: 'New York',
     urgent: false
   };
@@ -27,7 +29,7 @@ export class FlightSearchComponent {
     3: true,
     5: true
   };
-  protected flights$ = this.ticketsFacade.flights$;
+  protected flights: Flight[] = [];
 
   protected search(filter: FlightFilter): void {
     this.filter = filter;
@@ -36,7 +38,11 @@ export class FlightSearchComponent {
       return;
     }
 
-    this.ticketsFacade.search(this.filter);
+    this.flightService.find(
+      this.filter.from, this.filter.to, this.filter.urgent
+    ).subscribe(
+      flights => this.flights = flights
+    );
   }
 
   protected delay(flight: Flight): void {
@@ -50,10 +56,12 @@ export class FlightSearchComponent {
       delayed: true
     };
 
-    this.ticketsFacade.update(newFlight);
+    this.flights = this.flights.map(
+      flight => flight.id === newFlight.id ? newFlight : flight
+    );
   }
 
   protected reset(): void {
-    this.ticketsFacade.reset();
+    this.flights = [];
   }
 }
