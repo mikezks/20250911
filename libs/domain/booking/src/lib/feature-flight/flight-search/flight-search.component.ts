366d488bb39ce1a1de290c1aeb5ee5f3ec29e732
Michael Egger-Zikes <23002661+mikezks@users.noreply.github.com>
2025-09-11 11:05:02 +0200

    demo: glitch-free behavior

--- libs/domain/booking/src/lib/feature-flight/flight-search/flight-search.component.ts
+++ libs/domain/booking/src/lib/feature-flight/flight-search/flight-search.component.ts
@@ -46,6 +46,15 @@ export class FlightSearchComponent {
     console.log(this.filter().from);
     this.filter.update(curr => ({ ...curr, from: 'Oslo' }));
     console.log(this.filter().from);
+
+    // Glitch-free behavior
+    const counter = signal(0);
+    const isEven = computed(() => counter() % 2 === 0);
+    effect(() => console.log(({
+      counter: counter(),
+      isEven: isEven()
+    })));
+    setInterval(() => counter.update(curr => curr + 1), 1_000);
   }
 
   protected search(filter: FlightFilter): void {
