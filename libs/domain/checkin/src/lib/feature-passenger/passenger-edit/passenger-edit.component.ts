462910bfd8099d0f58b0f1a7e82b87af4a70bfa1
Michael Egger-Zikes <23002661+mikezks@users.noreply.github.com>
2025-09-11 11:54:32 +0200

    refactor: rxjs signal interop to observable, to signal

--- libs/domain/checkin/src/lib/feature-passenger/passenger-edit/passenger-edit.component.ts
+++ libs/domain/checkin/src/lib/feature-passenger/passenger-edit/passenger-edit.component.ts
@@ -1,6 +1,10 @@
-import { Component, inject } from '@angular/core';
+import { Component, effect, inject, input, numberAttribute, signal } from '@angular/core';
 import { NonNullableFormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { validatePassengerStatus } from '../../util-validation';
+import { initialPassenger } from '../../logic-passenger';
+import { toObservable, toSignal } from '@angular/core/rxjs-interop';
+import { switchMap } from 'rxjs';
+import { PassengerService } from '../../logic-passenger/data-access/passenger.service';
 
 
 @Component({
@@ -11,6 +15,7 @@ import { validatePassengerStatus } from '../../util-validation';
   templateUrl: './passenger-edit.component.html'
 })
 export class PassengerEditComponent {
+  private passengerService = inject(PassengerService);
   protected editForm = inject(NonNullableFormBuilder).group({
     id: [0],
     firstName: [''],
@@ -20,6 +25,27 @@ export class PassengerEditComponent {
       validatePassengerStatus(['A', 'B', 'C'])
     ]]
   });
+
+  id = input(0, { transform: numberAttribute });
+  private id$ = toObservable(this.id);
+  private passenger$ = this.id$.pipe(
+    switchMap(id => this.passengerService.findById(id))
+  );
+  private passenger = toSignal(this.passenger$, {
+    // requireSync: true,
+    initialValue: initialPassenger
+  });
+  // private passenger = signal(initialPassenger);
+
+  constructor() {
+    effect(() => console.log(this.id()));
+    effect(() => this.editForm.patchValue(this.passenger()));
+
+    /* setTimeout(() => this.passenger.set({
+      ...initialPassenger,
+      firstName: 'Mary'
+    }), 3_000); */
+  }
 
   protected save(): void {
     console.log(this.editForm.value);
