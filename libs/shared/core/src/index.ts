54666ae9a15654d0d1f727b46612af9f2d1c7142
Michael Egger-Zikes <23002661+mikezks@users.noreply.github.com>
2025-09-11 08:59:19 +0200

    init: modern angular

---
+++ libs/shared/core/src/index.ts
@@ -0,0 +1,8 @@
+/*
+ * Public API Surface of core
+ */
+
+export * from './lib/feature-core';
+export * from './lib/logic-communication';
+export * from './lib/ui-core';
+export * from './lib/util-cd-visualizer';
