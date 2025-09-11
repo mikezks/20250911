6935344163a5b07592fb94ff1c8f311490061807
Michael Egger-Zikes <23002661+mikezks@users.noreply.github.com>
2025-09-11 10:13:25 +0200

    refactor: smart component to signal, computed, effect

--- libs/domain/booking/src/lib/feature-flight/flight-search/flight-search.component.ts
+++ libs/domain/booking/src/lib/feature-flight/flight-search/flight-search.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, computed, effect, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Flight, FlightFilter, injectTicketsFacade } from '../../logic-flight';
 import { FlightCardComponent, FlightFilterComponent } from '../../ui-flight';
@@ -18,25 +18,32 @@ import { FlightCardComponent, FlightFilterComponent } from '../../ui-flight';
 export class FlightSearchComponent {
   private ticketsFacade = injectTicketsFacade();
 
-  protected filter = {
+  protected filter = signal({
     from: 'Paris',
     to: 'New York',
     urgent: false
-  };
+  });
+  protected route = computed(
+    () => 'From ' + this.filter().from + ' to ' + this.filter().to + '.'
+  );
   protected basket: Record<number, boolean> = {
     3: true,
     5: true
   };
   protected flights$ = this.ticketsFacade.flights$;
 
-  protected search(filter: FlightFilter): void {
-    this.filter = filter;
+  constructor() {
+    effect(() => console.log(this.route()));
+  }
 
-    if (!this.filter.from || !this.filter.to) {
+  protected search(filter: FlightFilter): void {
+    this.filter.set(filter);
+
+    if (!this.filter().from || !this.filter().to) {
       return;
     }
 
-    this.ticketsFacade.search(this.filter);
+    this.ticketsFacade.search(this.filter());
   }
 
   protected delay(flight: Flight): void {
