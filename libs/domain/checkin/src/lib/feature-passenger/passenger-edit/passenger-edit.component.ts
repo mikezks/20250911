aa1141cec5891888d7c598caf9745c42fabc064d
Michael Egger-Zikes <23002661+mikezks@users.noreply.github.com>
2025-09-12 09:26:54 +0200

    refactor: resource, rx resource

--- libs/domain/checkin/src/lib/feature-passenger/passenger-edit/passenger-edit.component.ts
+++ libs/domain/checkin/src/lib/feature-passenger/passenger-edit/passenger-edit.component.ts
@@ -1,16 +1,15 @@
-import { Component, effect, inject, input, numberAttribute, signal } from '@angular/core';
+import { Component, effect, inject, input, numberAttribute } from '@angular/core';
 import { NonNullableFormBuilder, ReactiveFormsModule } from '@angular/forms';
-import { validatePassengerStatus } from '../../util-validation';
-import { initialPassenger } from '../../logic-passenger';
-import { toObservable, toSignal } from '@angular/core/rxjs-interop';
-import { switchMap } from 'rxjs';
+import { RouterLink } from '@angular/router';
 import { PassengerService } from '../../logic-passenger/data-access/passenger.service';
+import { validatePassengerStatus } from '../../util-validation';
 
 
 @Component({
   selector: 'app-passenger-edit',
   imports: [
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterLink
   ],
   templateUrl: './passenger-edit.component.html'
 })
@@ -27,17 +26,18 @@ export class PassengerEditComponent {
   });
 
   id = input(0, { transform: numberAttribute });
-  private passenger = toSignal(
-    toObservable(this.id).pipe(
-      switchMap(id => this.passengerService.findById(id))
-    ), { initialValue: initialPassenger }
-  );
+  protected passengerResource = this.passengerService.findByIdAsResource(this.id);
 
   constructor() {
-    effect(() => this.editForm.patchValue(this.passenger()));
+    effect(() => {
+      if (this.passengerResource.hasValue()) {
+        this.editForm.patchValue(this.passengerResource.value());
+      }
+    });
   }
 
   protected save(): void {
     console.log(this.editForm.value);
+    this.passengerResource.set(this.editForm.getRawValue());
   }
 }
