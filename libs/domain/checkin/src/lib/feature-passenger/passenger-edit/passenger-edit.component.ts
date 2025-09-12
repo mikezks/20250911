be5de65cb376fc9ab27e9affa49887238f56fa0c
Michael Egger-Zikes <23002661+mikezks@users.noreply.github.com>
2025-09-12 10:35:21 +0200

    refactor: simplify component, add schema with required validator

--- libs/domain/checkin/src/lib/feature-passenger/passenger-edit/passenger-edit.component.ts
+++ libs/domain/checkin/src/lib/feature-passenger/passenger-edit/passenger-edit.component.ts
@@ -1,14 +1,15 @@
-import { Component, effect, inject, input, numberAttribute, signal } from '@angular/core';
-import { NonNullableFormBuilder, ReactiveFormsModule } from '@angular/forms';
-import { Control, form } from '@angular/forms/signals';
-import { RouterLink } from '@angular/router';
-import { PassengerService } from '../../logic-passenger/data-access/passenger.service';
-import { validatePassengerStatus } from '../../util-validation';
 import { httpResource } from '@angular/common/http';
+import { Component, input, numberAttribute } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { Control, form, required, schema } from '@angular/forms/signals';
+import { RouterLink } from '@angular/router';
 import { initialPassenger, Passenger } from '../../logic-passenger';
 
 
 // (3) Form Logic: Schema w/ validators, disabled, readonly, hidden, etc.
+const passengerSchema = schema<Passenger>(passengerPath => {
+  required(passengerPath.passengerStatus);
+});
 
 
 @Component({
@@ -23,28 +24,19 @@ import { initialPassenger, Passenger } from '../../logic-passenger';
 })
 export class PassengerEditComponent {
   id = input(0, { transform: numberAttribute });
+
+  // (1) Data Model: Writable Signal
   protected passengerResource = httpResource<Passenger>(() => ({
     url: 'https://demo.angulararchitects.io/api/passenger',
     params: {
       id: this.id()
     }
   }), { defaultValue: initialPassenger });
-  // (1) Data Model: Writable Signal
-  // passengerState = signal(initialPassenger);
-
+  
   // (2) Form State: valid, dirty, touched, value
-  editForm = form(this.passengerResource.value);
-
-  constructor() {
-    effect(() => {
-      if (this.passengerResource.hasValue()) {
-        // this.editForm.patchValue(this.passengerResource.value());
-      }
-    });
-  }
+  editForm = form(this.passengerResource.value, passengerSchema);
 
   protected save(): void {
     console.log(this.editForm().value());
-    // this.passengerResource.set(this.editForm.getRawValue());
   }
 }
