ae0b763a32786610fe8c094f0db84ec2cff4f857
Michael Egger-Zikes <23002661+mikezks@users.noreply.github.com>
2025-09-12 18:04:28 +0200

    refactor: integrate basket, selection change

--- libs/domain/booking/src/lib/feature-flight/flight-search/flight-search.component.ts
+++ libs/domain/booking/src/lib/feature-flight/flight-search/flight-search.component.ts
@@ -2,7 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { injectDispatch } from '@ngrx/signals/events';
-import { BookingStore, Flight } from '../../logic-flight';
+import { BookingStore } from '../../logic-flight';
 import { flightEvents } from '../../logic-flight/state/flight.events';
 import { FlightCardComponent, FlightFilterComponent } from '../../ui-flight';
 
@@ -21,20 +21,4 @@ import { FlightCardComponent, FlightFilterComponent } from '../../ui-flight';
 export class FlightSearchComponent {
   protected store = inject(BookingStore);
   protected flightEvents = injectDispatch(flightEvents);
-
-  protected delay(flight: Flight): void {
-    const oldFlight = flight;
-    const oldDate = new Date(oldFlight.date);
-
-    const newDate = new Date(oldDate.getTime() + 1000 * 60 * 5); // Add 5 min
-    const newFlight = {
-      ...oldFlight,
-      date: newDate.toISOString(),
-      delayed: true
-    };
-
-    this.flightEvents.flightsLoaded(this.store.flightEntities().map(
-      flight => flight.id === newFlight.id ? newFlight : flight
-    ));
-  }
 }
